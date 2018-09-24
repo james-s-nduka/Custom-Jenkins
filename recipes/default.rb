638f157c8a81115a729637b7c7bb35af697cdcdc
JNduka <james.s.nduka@hotmail.com>
2018-09-24 16:13:48 +0100

    addition of dependent plugins

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,11 +5,43 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 include_recipe 'jenkins::master'
-include_recipe 'java' unless system('which java')
+
+# Install java which is required for jenkins if the machine doesn't already have it available
+include_recipe 'java' unless system('java -version')
 
 # Install the latest version of the greenballs plugin
 jenkins_plugin 'greenballs' do
   source 'http://updates.jenkins-ci.org/download/plugins/greenballs/1.15/greenballs.hpi'
+end
+
+# Install the latest version of the display-url-api plugin
+jenkins_plugin 'display-url-api' do
+  source 'http://updates.jenkins-ci.org/download/plugins/display-url-api/2.0/display-url-api.hpi'
+end
+
+# Install the latest version of the junit plugin
+jenkins_plugin 'junit' do
+  source 'http://updates.jenkins-ci.org/download/plugins/junit/1.25/junit.hpi'
+end
+
+# Install the latest version of the icon-shim plugin
+jenkins_plugin 'icon-shim' do
+  source 'http://updates.jenkins-ci.org/download/plugins/icon-shim/2.0.2/icon-shim.hpi'
+end
+
+# Install the latest version of the credenials plugin
+jenkins_plugin 'credentials' do
+  source 'http://updates.jenkins-ci.org/download/plugins/credentials/2.1.18/credentials.hpi'
+end
+
+# Install the latest version of the plain-credentials plugin
+jenkins_plugin 'plain-credentials' do
+  source 'http://updates.jenkins-ci.org/download/plugins/plain-credentials/1.4/plain-credentials.hpi'
+end
+
+# Install the latest version of the workflow-step-api plugin
+jenkins_plugin 'workflow-step-api' do
+  source 'http://updates.jenkins-ci.org/download/plugins/workflow-step-api/2.16/workflow-step-api.hpi'
 end
 
 # Install the latest version of the mailer plugin
